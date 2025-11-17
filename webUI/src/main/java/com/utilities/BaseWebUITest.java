474b95f2db2040cf659422fe9fd216ee0b646af3
ronylevi <ronylevi99@gmail.com>
2025-11-17 10:36:50 +0200

    webUI parallel support

--- webUI/src/main/java/com/utilities/BaseWebUITest.java
+++ webUI/src/main/java/com/utilities/BaseWebUITest.java
@@ -7,25 +7,44 @@ import static com.utilities.Ansi.*;
 
 public class BaseWebUITest {
 
-    protected static Playwright playwright;
-    protected static Browser browser;
+    protected Playwright playwright;
+    protected Browser browser;
     protected BrowserContext context;
     protected static Page page;
 
+    @Parameters("browserName")
     @BeforeClass(alwaysRun = true)
-    public void launchBrowser() {
-        System.out.println(BLUE + "<<<  Before class  >>>" + RESET);
+    public void launchBrowser(@Optional("chromium") String browserName) {
+        System.out.println(BLUE + "<<<  Before class  >>> browser=" + browserName + RESET);
+
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false)
-        );
+
+        BrowserType.LaunchOptions options = new BrowserType.LaunchOptions()
+                .setHeadless(false); // set true on CI if needed
+
+        switch (browserName.toLowerCase()) {
+            case "firefox":
+                browser = playwright.firefox().launch(options);
+                break;
+            case "webkit":
+                browser = playwright.webkit().launch(options);
+                break;
+            case "chromium":
+            default:
+                browser = playwright.chromium().launch(options);
+                break;
+        }
     }
 
     @AfterClass(alwaysRun = true)
     public void closeBrowser() {
         System.out.println(BLUE + "Closing browser" + RESET);
-        if (browser != null) browser.close();
-        if (playwright != null) playwright.close();
+        if (browser != null) {
+            browser.close();
+        }
+        if (playwright != null) {
+            playwright.close();
+        }
     }
 
     @BeforeMethod(alwaysRun = true)
@@ -38,6 +57,8 @@ public class BaseWebUITest {
 
     @AfterMethod(alwaysRun = true)
     public void closeContext() {
-        if (context != null) context.close();
+        if (context != null) {
+            context.close();
+        }
     }
 }
